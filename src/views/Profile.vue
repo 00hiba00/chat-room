b130a1fa5b6d0f635a5e45dd328af8aad3c70c8b
00hiba00 <hibaelbouanani123@gmail.com>
2025-04-14 11:22:56 +0100

    structure+user authentification

---
+++ src/views/Profile.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Mon Profil</h2>
+        <user-profile></user-profile>
+        <router-link to="/Discussion">go to discussions</router-link>
+    </div>
+</template>
+
+<script setup>
+    import UserProfile from '../components/UserProfile.vue';
+</script>
